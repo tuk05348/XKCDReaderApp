2ff851afe36f420047057bff95e1e8dfb5a6c1a5
Karl Morris <karl.morris@temple.edu>
2022-03-24 20:45:45 -0400

    Volley implementation

--- app/src/main/java/edu/temple/xkcdreader/MainActivity.kt
+++ app/src/main/java/edu/temple/xkcdreader/MainActivity.kt
@@ -7,13 +7,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.lifecycle.lifecycleScope
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONObject
-import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var titleTextView: TextView
     lateinit var altTextView: TextView
     lateinit var comicImageView: ImageView
+
+    lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,31 +34,30 @@ class MainActivity : AppCompatActivity() {
         altTextView = findViewById(R.id.altTextView)
         comicImageView = findViewById(R.id.comicImageView)
 
+        requestQueue = Volley.newRequestQueue(this)
+
         fetchComicButton.setOnClickListener{
-            lifecycleScope.launch(Dispatchers.Main) {
-                fetchComic(comicNumberEditText.text.toString())
-            }
+            fetchComic(comicNumberEditText.text.toString())
         }
 
 
     }
 
-    suspend fun fetchComic(comicId: String) {
-
-        val jsonObject: JSONObject
-
-        withContext(Dispatchers.IO) {
-            jsonObject = JSONObject(URL("https://xkcd.com/$comicId/info.0.json")
-                .openStream()
-                .bufferedReader()
-                .readLine())
-        }
-
-        titleTextView.text = jsonObject.getString("safe_title")
-        altTextView.text = jsonObject.getString("alt")
-        comicImageView.contentDescription = jsonObject.getString("transcript")
-        Picasso.get().load(jsonObject.getString("img")).into(comicImageView)
-
+    fun fetchComic(comicId: String) {
+        requestQueue.add(
+            JsonObjectRequest(Request.Method.GET,
+            "https://xkcd.com/$comicId/info.0.json",
+            null,
+                {jsonObject ->
+                    titleTextView.text = jsonObject.getString("safe_title")
+                    altTextView.text = jsonObject.getString("alt")
+                    comicImageView.contentDescription = jsonObject.getString("transcript")
+                    Picasso.get().load(jsonObject.getString("img")).into(comicImageView)
+                },
+                {
+                    Log.e("Server Response Error", it.toString())
+                })
+        )
     }
 
 }
